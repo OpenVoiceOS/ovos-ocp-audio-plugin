f6b0dc8082672f6c5f092be7ffe2d0f4b0520882
jarbasal <jarbasai@mailfence.com>
2021-07-22 22:25:52 +0100

    gui player plugin
    ported from https://github.com/MycroftAI/mycroft-core/pull/2952

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+PLUGIN_ENTRY_POINT = 'ovos_guiplayer=ovos_plugin_guiplayer'
+
+setup(
+    name='ovos_plugin_guiplayer',
+    version='0.0.1a1',
+    description='guiplayer plugin for ovos',
+    url='https://github.com/OpenVoiceOS/ovos-guiplayer-plugin',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    packages=['ovos_plugin_guiplayer'],
+    install_requires=["ovos-plugin-manager>=0.0.1a3"],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='ovos audio plugin',
+    entry_points={'mycroft.plugin.audioservice': PLUGIN_ENTRY_POINT}
+)
