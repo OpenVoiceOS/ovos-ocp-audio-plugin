5b32d1404943d440db9d2e3bedb51eaffb00ab64
jarbasai <jarbasai@mailfence.com>
2022-01-14 13:37:30 +0000

    pls detect fix

--- ovos_plugin_common_play/ocp/stream_handlers/playlists.py
+++ ovos_plugin_common_play/ocp/stream_handlers/playlists.py
@@ -3,7 +3,7 @@ import requests
 
 def get_playlist_stream(uri):
     # .pls and .m3u are not supported by gui player, parse the file
-    if ".pls" in uri or ".m3u" in uri:
+    if "pls" in uri or ".m3u" in uri:
         txt = requests.get(uri).text
         for l in txt.split("\n"):
             if l.startswith("http"):
