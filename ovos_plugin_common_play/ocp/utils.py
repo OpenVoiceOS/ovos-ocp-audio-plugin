c0f8817e1720d427127413363e6b394ddce7e395
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-01-10 18:57:00 +0000

    Feat/ocp stream plugins (#27)
    * ocp stream extractors
    
    * ocp stream extractors
    
    * ocp stream extractors
    
    * plugins
    
    * plugins
    
    * plugins
    
    * ocp metadata plugin
    
    * missing import
    
    * bundle plugins for compat
    
    * bundle plugins for compat
    
    * min opm
    
    * min opm

--- ovos_plugin_common_play/ocp/utils.py
+++ ovos_plugin_common_play/ocp/utils.py
@@ -1,51 +1,32 @@
+import mimetypes
 import shutil
 import tempfile
 from os import makedirs
 from os.path import basename, expanduser, isfile, join, dirname
 
 from ovos_plugin_common_play.ocp.status import TrackState, PlaybackType
+from ovos_plugin_manager.ocp import StreamHandler
+from ovos_ocp_files_plugin.plugin import OCPFilesMetadataExtractor
+ocp_plugins = StreamHandler()
+
+
+def find_mime(uri):
+    """ Determine mime type. """
+    mime = mimetypes.guess_type(uri)
+    if mime:
+        return mime
+    else:
+        return None
+
+
+def available_extractors():
+    return ["/", "http"] + \
+           [f"{sei}//" for sei in ocp_plugins.supported_seis]
 
 
 def extract_metadata(uri):
-    try:
-        import audio_metadata
-    except ImportError:  # common conflicts with attrs version.... replace ASAP
-        audio_metadata = None
-
-    meta = {"uri": uri,
-            "title": basename(uri),
-            "playback": PlaybackType.AUDIO,
-            "status": TrackState.DISAMBIGUATION}
-    if not audio_metadata:
-        return meta
-    m = audio_metadata.load(uri.replace("file://", ""))
-    if m.tags:
-        if m.tags.get("title"):
-            meta["title"] = m.tags.title[0]
-        if m.tags.get("album"):
-            meta["album"] = m.tags.album[0]
-
-        if m.tags.get("artist"):
-            meta["artist"] = m.tags.artist[0]
-        elif m.tags.get("composer"):
-            meta["artist"] = m.tags.composer[0]
-
-        if m.tags.get("date"):
-            meta["date"] = m.tags.date[0]
-        if m.tags.get("audiolength"):
-            meta["duration"] = m.tags.audiolength[0]
-        if m.tags.get("genre"):
-            meta["genre"] = m.tags.genre[0]
-
-    if m.pictures:
-        try:
-            img_path = f"{tempfile.gettempdir()}/{meta['title']}.jpg"
-            with open(img_path, "wb") as f:
-                f.write(m.pictures[0].data)
-            meta["image"]: img_path
-        except:
-            pass
-    return meta
+    # backwards compat
+    return OCPFilesMetadataExtractor.extract_metadata(uri)
 
 
 def create_desktop_file():
