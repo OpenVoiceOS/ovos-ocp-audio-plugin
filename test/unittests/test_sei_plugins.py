ee97a0718bd4814caf41d7741b4e3ea8f9ad7220
jarbasai <jarbasai@mailfence.com>
2023-02-17 04:15:50 +0000

    .

--- test/unittests/test_sei_plugins.py
+++ test/unittests/test_sei_plugins.py
@@ -5,7 +5,7 @@ from ovos_plugin_manager.ocp import StreamHandler
 
 class TestOCPExtractor(unittest.TestCase):
 
-    def test_news(self):
+    def test_rss(self):
         parser = StreamHandler()
         rss_urls = [
             "https://www.spreaker.com/show/1401466/episodes/feed",
@@ -15,7 +15,7 @@ class TestOCPExtractor(unittest.TestCase):
             "https://podcasts.files.bbci.co.uk/p02nq0gn.rss",
             "http://www.rtp.pt/play/podcast/7496",
             "https://www.cbc.ca/podcasting/includes/hourlynews.xml",
-            "https://podcast.hr-online.de/der_tag_in_hessen/podcast.xm",
+            "https://podcast.hr-online.de/der_tag_in_hessen/podcast.xml",
             "https://api.sr.se/api/rss/pod/3795",
             "http://api.rtve.es/api/programas/36019/audios.rs",
             "https://www.pbs.org/newshour/feeds/rss/podcasts/show",
@@ -23,16 +23,16 @@ class TestOCPExtractor(unittest.TestCase):
             "https://de1.api.radio-browser.info/pls/url/69bc7084-523c-11ea-be63-52543be04c81"  # .pls
         ]
         for url in rss_urls:
-            meta = parser.extract_stream(url)
+            print(f"#### {url}")
+            meta = parser.extract_stream(f"rss//{url}") or {}
             self.assertTrue(bool(meta.get("uri")))
-            meta = parser.extract_stream(f"rss//{url}")
-            self.assertTrue(bool(meta.get("uri")))
-            meta = parser.extract_stream(f"news//{url}")
-            self.assertTrue(bool(meta.get("uri")))
-            meta = parser.extract_stream(f"news//rss//{url}")
+            # test dropped news// sei
+            meta = parser.extract_stream(f"news//rss//{url}") or {}
             self.assertTrue(bool(meta.get("uri")))
 
-        parser_urls = [
+    def test_news(self):
+        parser = StreamHandler()
+        news_urls = [
             "https://www.raiplaysound.it",
             "https://www.tsf.pt/stream",
             "https://www.abc.net.au/news",
@@ -40,12 +40,12 @@ class TestOCPExtractor(unittest.TestCase):
             "https://www.npr.org/rss/podcast.php",
             "http://feeds.feedburner.com/gpbnews"
         ]
-        for url in parser_urls:
-            meta = parser.extract_stream(url)
+        for url in news_urls:
+            print(f"#### {url}")
+            meta = parser.extract_stream(url) or {}
             self.assertTrue(bool(meta.get("uri")))
-            meta = parser.extract_stream(f"news//{url}")
+            meta = parser.extract_stream(f"news//{url}") or {}
             self.assertTrue(bool(meta.get("uri")))
-
 
 
 if __name__ == '__main__':
