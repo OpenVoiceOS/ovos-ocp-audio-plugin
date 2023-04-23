68e175e9ed13ff811265f8de410ed7402335a029
Gaëtan Trellu <gaetan.trellu@gmail.com>
2023-04-22 20:21:20 -0400

    Replace mycroft_bus_client with ovos_bus_client (#80)

--- ovos_plugin_common_play/ocp/media.py
+++ ovos_plugin_common_play/ocp/media.py
@@ -1,6 +1,6 @@
 from typing import Optional, Tuple, List, Union
 
-from mycroft_bus_client import MessageBusClient
+from ovos_bus_client.client import MessageBusClient
 from ovos_plugin_common_play.ocp import OCP_ID
 from ovos_plugin_common_play.ocp.status import *
 from ovos_plugin_common_play.ocp.utils import ocp_plugins, find_mime
